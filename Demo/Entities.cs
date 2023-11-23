657171c69bfc072c5d81d5aa47d8298535d6fea3
Oleksandr Trukhin <trukhino@ou.org>
2023-11-23 22:00:35 +0100

    init

---
+++ Demo/Entities.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Demo
+{
+    public enum Currency
+    {
+        USD,
+        EUR
+    }
+    [Serializable]
+    public class Account : IComparable<Account>
+    {
+        public int OwnerCode { get; set; }
+        public double Amount { get; set; }
+        public Account() { }
+        [JsonConstructor]
+        public Account(int ownerCode, double amount)
+        {
+            OwnerCode = ownerCode;
+            Amount = amount;
+        }
+        public void TopUp(double AmountToTopup)
+        {
+            Amount += AmountToTopup;
+        }
+        public void Withdraw(double WithdrawalAmount)
+        {
+            Amount -= WithdrawalAmount;
+        }
+        public void TransferTo(double AmountToSend, Account Receiver)
+        {
+            Receiver.Amount += AmountToSend;
+            Amount -= AmountToSend;
+        }
+        public double ConvertTo(Currency TargetCurrency, IDictionary<Currency, double> ExchangeRate)
+        {
+            return Amount * ExchangeRate[TargetCurrency];
+        }
+        public int CompareTo(Account? other)
+        {
+            return OwnerCode.CompareTo(other?.OwnerCode);
+        }
+        public override string ToString()
+        {
+            return $"Account {OwnerCode} has {Amount}";
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> accounts = new()
+            {
+                new Account(1, 25),
+                new Account(25, 0.5),
+                new Account(345, 314),
+                new Account(9, 84)
+            };
+            JSONSerializer<Account> JSONSerializer = new("json.json");
+            JSONSerializer.Save(accounts);
+            Console.WriteLine("Saved to JSON:");
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine(account);
+            }
+            accounts = JSONSerializer.Load();
+            Console.WriteLine("Loaded from JSON:");
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine(account);
+            }
+
+
+            CustomSerializer<Account> CustomSerializer = new("custom.txt");
+            CustomSerializer.Save(accounts);
+            Console.WriteLine("Saved to Custom:");
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine(account);
+            }
+            accounts = CustomSerializer.Load();
+            Console.WriteLine("Loaded from Custom:");
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine(account);
+            }
+
+            XMLSerializer<Account> XMLSerializer = new("xml.xml");
+            XMLSerializer.Save(accounts);
+            Console.WriteLine("Saved to XML:");
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine(account);
+            }
+            accounts = XMLSerializer.Load();
+            Console.WriteLine("Loaded from XML:");
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine(account);
+            }
+
+            BinarySerializer<Account> BinarySerializer = new("binary.bin");
+            BinarySerializer.Save(accounts);
+            Console.WriteLine("Saved to Binary:");
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine(account);
+            }
+            accounts = BinarySerializer.Load();
+            Console.WriteLine("Loaded from Binary:");
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine(account);
+            }
+        }
+    }
+}
